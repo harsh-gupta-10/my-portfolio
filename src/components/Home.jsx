2fc9be81ddae91abaea3bb3c17479de9fb11c6c3
harsh-gupta-10 <harshgupta24716@gmail.com>
2024-11-22 21:26:26 +0530

    update logo image source in Home.jsx to use raw GitHub link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,7 +33,7 @@ return (
                     </div>
 
                     <div className='grid justify-center'>
-                            <img src="../../public/img/temp-logo.png" alt="" className="w-full max-w-xs md:max-w-md lg:max-w-lg"/>    
+                            <img src="https://github.com/harsh-gupta-10/my-portfolio/blob/main/public/img/temp-logo.png?raw=true" alt="" className="w-full max-w-xs md:max-w-md lg:max-w-lg"/>    
                     </div> 
             </div>
     </div>
